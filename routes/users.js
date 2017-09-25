84d9d8996752c7f0faffe038f397842b3598f556
Harshit Agarwal <h.agarwal1222@gmail.com>
2017-09-25 11:35:24 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+/*
+* GET userlist.
+*/
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
+});
+
+/*
+* POST users
+*/
+router.post('/adduser', function(req, res){
+  var db = req.db;
+  var collection =  db.get('userlist');
+  collection.insert(req.body, function (err, result) {
+    res.send(
+        (err === null) ? {msg: ''} : {msg: err}
+    );
+  });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  collection.remove({ '_id' : userToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+  });
+});
+
+
+
+module.exports = router;
